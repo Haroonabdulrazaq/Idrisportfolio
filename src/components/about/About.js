a4db1bafb3e03af52e1f38b232a00ce375f269c7
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-09-28 07:22:33 +0100

    Add tiers to about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import aboutData from './data';
+// import idrisdubai from '../../assets/images/Idrisdubai.jpeg';
 import "./about.scss";
 
 
 const About = () => {
   return (
     <div className="about-wrapper">
-      <div className="banner">
-        About page
-      </div>
+      <header className="banner">
+        <h1 className="banner-text">{aboutData.heading}</h1>
+      </header>
+      <main className="main-parent">
+        <div className="three-tier">
+          
+          <article className="tier text-desc">
+            <h2>
+              About Jimoh Idris
+            </h2>
+            <div>
+              {aboutData.about}
+            </div>
+          </article>
+          <article className="tier text-desc">
+            <h2 className="tier-headings">
+              About Jimoh Idris
+            </h2>
+            <div>
+              {aboutData.about}
+            </div>
+          </article>
+          <article className="tier capabilities">
+            <div className="digital-capabilities">
+              <h2 className="tier-heading" > Digital Capabilities </h2>
+              {aboutData.digital.map((capability)=> {
+                return (<div className="capability">
+                  <p>{capability}</p>
+                </div>)
+              })}
+            </div>
+            <div className="clients">
+              <h2 className="tier-heading "> Clients and collaborators </h2>
+              <div className="digital-capabilities">
+                {aboutData.digital.map((capability)=> {
+                  return (<div className="capability">
+                    <p>{capability}</p>
+                  </div>)
+                })}
+              </div>
+            </div>
+          </article>
+        </div>
+      </main>
     </div>
   )
 }
--- src/components/about/data.js
+++ src/components/about/data.js
@@ -1,5 +1,10 @@
 const aboutData = {
-  heading: "Jimoh helps define, design and launch world-class digital products."
+  heading: "Jimoh helps define, design and launch world-class digital products.",
+  desc: `I don't usually dress like this but I'm rather proud of this moment in my personal history so in the bio it goes. If you want to play PES or FIFA, see a movie, or grab a drink and talk design don't hesitate to engage me via mail or tweet.`,
+  about: `Jimoh is a Nigerian based product designer, UX researcher and writer. He’s worked on a variety in-house brands ranging from small non-profits to successful tech startups with ambitious goals. He has a unique drive at seeing the big picture potential in a product or brand and then bringing it to life.
+          More than 3 years of UX, Product Design, and leading creative teams has made me attempt solving design problems. I intentionally built my experience around design and business so I could confidently recommend intuitive, meaningful design experiences to clients - and their customers - across many different industries.
+          The results? Happy users and profitable businesses! Jimoh is comfortable working on a range of projects from designing native apps to collaborating with other designers. Whether he’s discusing requirements, illustrating on paper, or crafting prototypes on his laptop, Jimoh always brings a quality of work and creativity that is hard to match.`,
+  digital: ["Websites", "iOS and Android Apps", "Strategy", "Research", "E-commerce Design"],
 }
 
 export default aboutData;
