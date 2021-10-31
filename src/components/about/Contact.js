087d2f3c2d18df89ce1caeb3a2f60ac3fa185165
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-10-31 15:01:47 +0100

    Finished Contact page'

--- src/components/about/Contact.js
+++ src/components/about/Contact.js
@@ -6,11 +6,13 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <div className="contact">
-      <h1>CONTACT</h1>
-      <p className="work-together">Let's Work Together</p>
-      <div className="icons">
-        <p><AiFillMail title="Mail"/>jimohidris63@gmail.com</p>
-        <p><AiFillLinkedin title="LinkedIn"/>LinkedIn</p>
+      <h2>CONTACT</h2>
+      <div className="contact-detail">
+       <p  className="work-together">Let's Work Together</p>
+        <div className="icons">
+          <a href="mailto:jimohidris63@gmail.com" target="_blank" rel="noreferrer"><p><AiFillMail title="Mail"/>jimohidris63@gmail.com</p></a>
+          <a href="http://linkedin.com/in/jimoh-idris-237931b4" target="_blank" rel="noreferrer"><p><AiFillLinkedin title="LinkedIn"/>LinkedIn</p></a>
+        </div>
       </div>
     </div>
   )
