88c6b45c814230a24aa5f1b708fe07a6ce4832de
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-11-06 18:29:21 +0100

    Add color to social icon background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import Home from './components/home/Index';
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import {Route} from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <About/>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/about">
+        <About/>
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
     </div>
   );
 }
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Contact.scss";
+
+const Contact = () => {
+  return (
+    <div className="contact-wrapper">
+      <div className="top-contact">
+        <div className="hello"></div>
+        <div className="text"></div>
+        <div className="contacts-me">
+          <div className="contact-me"></div>
+          <div className="form"></div>
+        </div>
+      </div>
+      <div className="bottom-contact">
+        <div className="hello-text">
+          <div className="letters">
+            <span className="letter H">H</span>
+            <span className="letter E">E</span>
+            <span className="letter L1">L</span>
+            {/* <span className="letter L2">L</span>
+            <span className="letter O">O</span> */}
+          </div>
+        </div>
+        <div className="social-icons"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact;
